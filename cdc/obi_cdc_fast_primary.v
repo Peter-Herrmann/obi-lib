d2f9e4e0bdffc0503f3ba95bf5cac4dc4a9034e7
Peter-Herrmann <herrmannp7@gmail.com>
2023-10-24 11:49:17 -0700

    reset synchronizer, de-linting

--- cdc/obi_cdc_fast_primary.v
+++ cdc/obi_cdc_fast_primary.v
@@ -1,4 +1,16 @@
 `timescale 1ns/1ps
+///////////////////////////////////////////////////////////////////////////////
+//
+// Module Name: obi_cdc_fast_primary
+// Author: Peter Herrmann
+//
+// Description: This Open Bus Interface (OBI) clock domain crossing (CDC)
+//              supports a single operation at a time with no pipelining. 
+//              It only works for scenarios where the primary controller's
+//              clock domain is as fast or faster than the secondary device's
+//              clock domain.
+//
+///////////////////////////////////////////////////////////////////////////////
 
 module obi_cdc_fast_primary (
     input               rst_ni,
@@ -30,7 +42,9 @@ module obi_cdc_fast_primary (
         req_ff1, req_ff2,
         rvalid_ff1, 
         gnt_ack, gnt_ack_ff1, gnt_ack_ff2, gnt_ack_ff3,
-        gnt_ff1, gnt_ff2, gnt_ff3;
+        gnt_ff1, gnt_ff2, gnt_ff3,
+        rst_n_ctrl_ff1, rst_n_ctrl_ff2,
+        rst_n_secondary_ff1, rst_n_secondary_ff2;
 
     /////////////////////////
     // Transaction Tracker //
@@ -39,49 +53,32 @@ module obi_cdc_fast_primary (
     /* This module must account for the latency between a transaction being
        acccepted by the secondary bus and the primary bus receiving a grant. */
 
-    /* [x] While a transaction's grant is in flight, mask the req output.
-
-       [x] A transaction's grant starts when the req and gnt bits are high 
-           at the secondary port and no gnt is in flight, send a grant pulse.
-
-       [x] The receipt of a grant by the controller port must be pulse 
-           synchronized.
-
-       [x] Grant pulses sent by the secondary port must be separated by at least
-           one clock cycle to enable edge detection on the pulse synchronizer.
-
-       [x] When the primary receives a grant pulse, it asserts gnt_ack until 
-           gnt_in_flight is de-asserted. 
-
-       [x] The receipt of a grant acknowledgement by the secondary port must be
-           pulse synchronized.
-
-       [x] A transaction's grant is completed when the secondary port receives
-           acknowledgement that the grant was received. 
-
-       [x] The delay between the controller port receiving a grant and the 
-           controller port receiving a grant ack must be at least as long as
-           the pipeline delay of the req signal. */
+    /* verilator lint_off SYNCASYNCNET */
+    /* Verilator warning here is intended to protect agains mixing sync and
+       async reset signals, as a coding style. gnt_in_flight is not used in
+       this way, so the warning is inappropriate. */
 
     always @(posedge secondary_clk_i) begin
-        if ((gnt_ack_ff3 && !gnt_ack_ff2) || !rst_ni)
-            gnt_in_flight <= '0;
+        if ((gnt_ack_ff3 && !gnt_ack_ff2) || !rst_n_secondary_ff2)
+            gnt_in_flight <= 'b0;
         else if (req_ff2 && secondary_gnt_i && !gnt_in_flight)
-            gnt_in_flight <= '1;
+            gnt_in_flight <= 'b1;
     end
 
     always @(posedge ctrl_clk_i or negedge gnt_in_flight) begin
-        if(!rst_ni || !gnt_in_flight)
-            gnt_ack <= '0;
+        if(!rst_n_ctrl_ff2 || !gnt_in_flight)
+            gnt_ack <= 'b0;
         else if(gnt_ff3 && !gnt_ff2)
-            gnt_ack <= '1;
+            gnt_ack <= 'b1;
     end
 
+    /* verilator lint_on SYNCASYNCNET */
+
     always @(posedge secondary_clk_i) begin
-        if (!rst_ni) begin
-            gnt_ack_ff1 <= '0;
-            gnt_ack_ff2 <= '0;
-            gnt_ack_ff3 <= '0;
+        if (!rst_n_secondary_ff2) begin
+            gnt_ack_ff1 <= 'b0;
+            gnt_ack_ff2 <= 'b0;
+            gnt_ack_ff3 <= 'b0;
         end else begin
             gnt_ack_ff3 <= gnt_ack_ff2;
             gnt_ack_ff2 <= gnt_ack_ff1;
@@ -100,9 +97,9 @@ module obi_cdc_fast_primary (
     assign secondary_req_o   = req_ff2 && !gnt_in_flight;
 
     always @(posedge secondary_clk_i) begin
-        if (!rst_ni) begin
-            req_ff2 <= '0;
-            req_ff1 <= '0;
+        if (!rst_n_secondary_ff2) begin
+            req_ff2 <= 'b0;
+            req_ff1 <= 'b0;
         end else begin
             req_ff2 <= req_ff1;
             req_ff1 <= ctrl_req_i; 
@@ -117,21 +114,43 @@ module obi_cdc_fast_primary (
     assign ctrl_gnt_o = gnt_ff3 && !gnt_ff2;
 
     always @(posedge ctrl_clk_i) begin
-        if (!rst_ni) begin
-            gnt_ff3       <= '0;
-            gnt_ff2       <= '0;
-            gnt_ff1       <= '0;
-            ctrl_rvalid_o <= '0;
-            rvalid_ff1    <= '0;
+        if (!rst_n_ctrl_ff2) begin
+            gnt_ff3       <= 'b0;
+            gnt_ff2       <= 'b0;
+            gnt_ff1       <= 'b0;
+            ctrl_rvalid_o <= 'b0;
+            rvalid_ff1    <= 'b0;
         end else begin
             gnt_ff3       <= gnt_ff2;
             gnt_ff2       <= gnt_ff1;
             gnt_ff1       <= gnt_in_flight;
-
             ctrl_rvalid_o <= rvalid_ff1;
             rvalid_ff1    <= secondary_rvalid_i;
         end
+    end
 
+    /////////////////////////
+    // Reset Synchronizers //
+    /////////////////////////
+
+    always @(posedge ctrl_clk_i) begin
+        if (!rst_ni) begin
+            rst_n_ctrl_ff2 <= 'b0;
+            rst_n_ctrl_ff1 <= 'b0;
+        end else begin
+            rst_n_ctrl_ff2 <= rst_n_ctrl_ff1;
+            rst_n_ctrl_ff1 <= 'b1;
+        end
+    end
+
+    always @(posedge secondary_clk_i) begin
+        if (!rst_ni) begin
+            rst_n_secondary_ff2 <= 'b0;
+            rst_n_secondary_ff1 <= 'b0;
+        end else begin
+            rst_n_secondary_ff2 <= rst_n_secondary_ff1;
+            rst_n_secondary_ff1 <= 'b1;
+        end
     end
 
 endmodule
